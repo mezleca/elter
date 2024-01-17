473082c1b4b5121b558e1f47cd1068036717c21e
rel <92015390+mezleca@users.noreply.github.com>
2024-01-17 00:42:15 -0300

    Update History.js

--- src/utils/models/History.js
+++ src/utils/models/History.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 export const History = mongoose.model("elterAI", {
     timestamp: Number,
+    date: String,
     role: String,
     name: String,
     content: String
